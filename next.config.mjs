19fff732be2cdff35f703ba01ec5645c91931ea3
FABRIZE <fabrize@example.com>
2026-05-28 20:59:09 +0500

    Include ffmpeg binary for upload previews

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  outputFileTracingIncludes: {
+    "/api/uploads/[file]": ["./node_modules/ffmpeg-static/ffmpeg*"],
+  },
 };
 
 export default nextConfig;
